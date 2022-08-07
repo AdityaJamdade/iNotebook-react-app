4e1422a43c1c340ee4b749a385d26fb0c5947759
AdityaJamdade <adijamdade156@gmail.com>
2022-08-07 18:19:32 +0530

    made models, routes  functional

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,11 @@ const app = express();
 connectToMongo();
 const port = 3000;
 
+app.use(express.json())
+
 // Available routes
-app.get('/api/auth', require('./routes/auth'));
-app.get('/api/notes', require('./routes/notes'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/notes', require('./routes/notes'));
 
 app.listen(port, () => {
     console.log(`Example app listening on port http://localhost:${port}`)
